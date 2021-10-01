9d480806ef428a835d2feea222b3c9e429211d39
obito7621 <joshi7621@gmail.com>
2021-10-01 13:45:42 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import rootReducer from "./src/stores/rootReducer";
+
+import CustomDrawer from "./src/navigation/CustomDrawer";
+import { StatusBar } from "react-native";
+
+const Stack = createStackNavigator();
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
+const App = () => {
+  return (
+    <Provider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={{
+            headerShown: false,
+          }}
+          initialRouteName={"Home"}
+        >
+          <Stack.Screen name="Home" component={CustomDrawer} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
+  );
+};
+
+export default App;
